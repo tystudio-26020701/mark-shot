07cc828f1e27cee9e0fe43b5359f1c74b64faf00
tystudio-26020701 <tystudio-26020701@protonmail.com>
2026-07-31 23:42:22 +0800

    fix(build): guard pipewire_buffer_data_types without PipeWire headers
    The file is compiled unconditionally on Linux but includes spa/buffer/buffer.h,
    breaking builds on machines without libpipewire development files. Guard the
    implementation with HAVE_PIPEWIRE.

--- src/pipewire/pipewire_buffer_data_types.cpp
+++ src/pipewire/pipewire_buffer_data_types.cpp
@@ -1,21 +1,35 @@
 #include "pipewire/pipewire_buffer_data_types.h"
 
+#include <QtGlobal>
+
+#ifdef HAVE_PIPEWIRE
 #include <spa/buffer/buffer.h>
+#endif
 
 namespace markshot::pipewire {
 
 std::uint32_t bufferDataTypeMask(bool hasModifier)
 {
+#ifdef HAVE_PIPEWIRE
     return hasModifier
         ? (1u << SPA_DATA_DmaBuf)
         : ((1u << SPA_DATA_MemPtr) | (1u << SPA_DATA_MemFd));
+#else
+    Q_UNUSED(hasModifier);
+    return 0;
+#endif
 }
 
 std::array<bool, 2> modifierPreference(bool rawStreamMode)
 {
+#ifdef HAVE_PIPEWIRE
     return rawStreamMode
         ? std::array<bool, 2>{false, true}
         : std::array<bool, 2>{true, false};
+#else
+    Q_UNUSED(rawStreamMode);
+    return {false, false};
+#endif
 }
 
 }  // namespace markshot::pipewire
